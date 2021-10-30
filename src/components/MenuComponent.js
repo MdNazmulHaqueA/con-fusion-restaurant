339f2900981ac731b8d9775df6d1dd644ff25dcf
MdNazmulHaqueA <naz089fvrict@gmail.com>
2021-10-30 12:50:28 +0600

    Assignment1: DishdetailComponent created and display comments

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
-
-import {
-   Card,
-   CardImg,
-   CardImgOverlay,
-   CardText,
-   CardBody,
-   CardTitle,
-} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent';
 
 class Menu extends Component {
    constructor(props) {
@@ -16,47 +9,40 @@ class Menu extends Component {
       this.state = {
          selectedDish: null,
       };
+      console.log('Menu Component Constructor Invoked');
+   }
+
+   componentDidMount() {
+      console.log('Menu Component componentDidMount Invoked');
    }
 
    onDishSelect(dish) {
       this.setState({ selectedDish: dish });
    }
 
-   renderDish(dish) {
-      if (dish != null)
-         return (
-            <Card>
-               <CardImg top src={dish.image} alt={dish.name} />
-               <CardBody>
-                  <CardTitle>{dish.name}</CardTitle>
-                  <CardText>{dish.description}</CardText>
-               </CardBody>
-            </Card>
-         );
-      else return <div></div>;
-   }
-
    render() {
       const menu = this.props.dishes.map(dish => {
          return (
-            <div className="col-12 col-md-5 m-1">
-               <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+            <div className="col-12 col-md-5 m-1" key={dish.id}>
+               <Card onClick={() => this.onDishSelect(dish)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
-                     <CardTitle>{dish.name}</CardTitle>
+                     <CardTitle>
+                        <h4>{dish.name}</h4>
+                     </CardTitle>
                   </CardImgOverlay>
                </Card>
             </div>
          );
       });
+      console.log('Menu Component RENDER Invoked');
 
       return (
          <div className="container">
             <div className="row">{menu}</div>
             <div className="row">
-               <div className="col-12 col-md-5 m-1">
-                  {this.renderDish(this.state.selectedDish)}
-               </div>
+               {/* {this.renderDish(this.state.selectedDish)} */}
+               <DishDetail selectedDish={this.state.selectedDish} />
             </div>
          </div>
       );
