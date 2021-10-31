8949205f4e7dbeb94ef946f3ae42cc9cf1882140
MdNazmulHaqueA <naz089fvrict@gmail.com>
2021-10-31 09:59:08 +0600

    DishDetail Component is devided into 2 components in the same file

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,46 +2,59 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 function RenderDish({ dish }) {
-   if (dish != null)
+   const { image, name, description } = dish;
+   return (
+      <div className="col-12 col-md-5 m-1">
+         <Card>
+            <CardImg top src={image} alt={name} />
+            <CardBody>
+               <CardTitle>
+                  <h3>{name}</h3>
+               </CardTitle>
+               <CardText>{description}</CardText>
+            </CardBody>
+         </Card>
+      </div>
+   );
+}
+
+function RenderComment({ comments }) {
+   return (
+      <div className="col-12 col-md">
+         <h3>Comments</h3>
+         {comments &&
+            comments.map(commentDetails => {
+               const { id, comment, author, date } = commentDetails;
+               return (
+                  <div key={id}>
+                     <p>{comment}</p>
+                     <p>
+                        --{author} ,{' '}
+                        {new Intl.DateTimeFormat('en-US', {
+                           year: 'numeric',
+                           month: 'short',
+                           day: '2-digit',
+                        }).format(new Date(Date.parse(date)))}
+                     </p>
+                  </div>
+               );
+            })}
+      </div>
+   );
+}
+
+const DishDetail = ({ dish }) => {
+   if (dish) {
       return (
          <div className="container">
             <div className="row">
-               <div className="col-12 col-md-5 m-1">
-                  <Card>
-                     <CardImg top src={dish.image} alt={dish.name} />
-                     <CardBody>
-                        <CardTitle>
-                           <h3>{dish.name}</h3>
-                        </CardTitle>
-                        <CardText>{dish.description}</CardText>
-                     </CardBody>
-                  </Card>
-               </div>
-               <div className="col-12 col-md">
-                  <h3>Comments</h3>
-                  {dish.comments.map(commentDetails => {
-                     const { id, comment, author, date } = commentDetails;
-                     return (
-                        <div key={id}>
-                           <p>{comment}</p>
-                           <p>
-                              --{author} ,{' '}
-                              {new Intl.DateTimeFormat('en-US', {
-                                 year: 'numeric',
-                                 month: 'short',
-                                 day: '2-digit',
-                              }).format(new Date(Date.parse(date)))}
-                           </p>
-                        </div>
-                     );
-                  })}
-               </div>
+               <RenderDish dish={dish} />
+               <RenderComment comments={dish.comments} />
             </div>
          </div>
       );
-   else return <div></div>;
-}
-
-const DishDetail = ({ dish }) => <RenderDish dish={dish} />;
-
+   } else {
+      return null;
+   }
+};
 export default DishDetail;
