ef14a8a31b77b5dd2893421ae53b69439260ab95
MdNazmulHaqueA <naz089fvrict@gmail.com>
2021-11-04 12:32:37 +0600

    SPA part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import {
+   Card,
+   CardImg,
+   CardText,
+   CardBody,
+   CardTitle,
+   Breadcrumb,
+   BreadcrumbItem,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({ dish }) {
    const { image, name, description } = dish;
@@ -43,18 +52,42 @@ function RenderComments({ comments }) {
    );
 }
 
-const DishDetail = ({ dish }) => {
-   if (dish) {
-      return (
-         <div className="container">
-            <div className="row">
-               <RenderDish dish={dish} />
-               <RenderComments comments={dish.comments} />
+const DishDetail = props => {
+   // if (dish) {
+   //    return (
+   //       <div className="container">
+   //          <div className="row">
+   //             <RenderDish dish={dish} />
+   //             <RenderComments comments={dish.comments} />
+   //          </div>
+   //       </div>
+   //    );
+   // } else {
+   //    return null;
+   // }
+   return (
+      <div className="container">
+         <div className="row">
+            <Breadcrumb>
+               <BreadcrumbItem>
+                  <Link to="/menu">Menu</Link>
+               </BreadcrumbItem>
+               <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+               <h3>{props.dish.name}</h3>
+               <hr />
             </div>
          </div>
-      );
-   } else {
-      return null;
-   }
+         <div className="row">
+            <div className="col-12 col-md-5 m-1">
+               <RenderDish dish={props.dish} />
+            </div>
+            <div className="col-12 col-md-5 m-1">
+               <RenderComments comments={props.comments} />
+            </div>
+         </div>
+      </div>
+   );
 };
 export default DishDetail;
